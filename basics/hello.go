e1ed04d340582aff583f247dba03616a1a67f5a1
Iyeba Kallon <iyebalphakallon@gmail.com>
2026-02-24 18:32:10 +0000

    feat: Add initial Go 'Hello, World!' program and a file for variables examples.
    Signed-off-by: Iyeba Kallon <iyebalphakallon@gmail.com>

---
+++ basics/hello.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// print to console
+	fmt.Println("Hello, World!")	
+
+
+	// print with formating
+
+	fmt.Printf("My name is %s and i am %d years old\n", "Riya", 21)
+
+	// print without new line
+	fmt.Print("Hello ")
+	fmt.Println("World\n")
+}
